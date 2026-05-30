de1f4cb186ebb9de2d5e734076f9673eb7927147
shivr-dev <shivrdream@gmail.com>
2026-05-30 17:25:44 +0800

    Delete js directory

--- js/11-settings.js
+++
@@ -1,7 +0,0 @@
-    function showFinishPlaceholder() {}
-    function toggleDblClick() { allowDblClickClear = document.getElementById('dblclick-toggle').checked; localStorage.setItem('allow_dblclick', allowDblClickClear); }
-    function toggleFullScreen() {
-      if (!document.fullscreenElement) document.documentElement.requestFullscreen().catch(err => showToast(`全屏失败: ${err.message}`));
-      else if (document.exitFullscreen) document.exitFullscreen();
-      closeAllPanels();
-    }
